46510b769cd2be04514f33bc0d2fa8a59dc3ef92
Ben Garcia <ben@seebenprogram.com>
2018-03-31 21:58:51 -0400

    Update build path

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/Portfolio-Page-v2/',
+    assetsPublicPath: '/',
 
     /**
      * Source Maps
