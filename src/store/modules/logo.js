6923d08b8b40cbaaf7e7666ff58c1fcd0843b777
Ben Garcia <ben@seebenprogram.com>
2018-04-01 22:39:33 -0400

    add module data

--- src/store/modules/logo.js
+++ src/store/modules/logo.js
@@ -1,6 +1,11 @@
 const state = {
   _logo: {
-
+    imgSrc: require('../../assets/images/profile.jpeg'),
+    imgAlt: 'Profile picture of Benjamin Garcia, Full Stack Web Developer',
+    name: 'Benjamin<br>Garcia',
+    title: 'Full Stack Developer',
+    emailAddr: 'ben@SeeBenProgram.com',
+    mailto: 'mailto:ben@SeeBenProgram.com?Subject=I%20want%20to%20hire%20you%21'
   }
 }
 
