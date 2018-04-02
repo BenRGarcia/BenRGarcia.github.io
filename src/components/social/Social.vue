89da4a61063b7a63a4d4d47e30399d4bd9b733b2
Ben Garcia <ben@seebenprogram.com>
2018-04-02 13:33:57 -0400

    Build Social component

--- src/components/social/Social.vue
+++ src/components/social/Social.vue
@@ -1,17 +1,23 @@
 <template>
-  <div class="card">
-    <div class="card-body">
-      <div>
-        <a
-          v-for="(link, index) in socialLinks"
-          :key="index"
-          :href="link.href"
-        >
-          <img
-            :src="link.imgSrc"
-            :alt="link.imgAlt"
-          >
-        </a>
+  <div class="row">
+    <div :class="css.grid">
+      <div class="card">
+        <div class="card-body text-center px-0">
+          <div>
+            <a
+              v-for="(link, index) in socialLinks"
+              :key="index"
+              :href="link.href"
+              target="_blank"
+            >
+              <img
+                :src="link.imgSrc"
+                :alt="link.imgAlt"
+                class="mx-3"
+              >
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -19,6 +25,17 @@
 
 <script>
 export default {
+  data () {
+    return {
+      css: {
+        grid: [
+          'col-8', 'offset-2',
+          'col-sm-6', 'offset-sm-3',
+          'col-md-4', 'offset-md-4'
+        ]
+      }
+    }
+  },
   computed: {
     socialLinks () {
       return this.$store.getters['social/getSocial']
@@ -43,5 +60,8 @@ a:hover {
   border-radius: 20px;
   box-shadow: 4px 4px 10px 0 rgba(0, 0, 0, 0.15),
               6px 8px 18px 0 rgba(0, 0, 0, 0.10);
+}
+img {
+  height: 50px;
 }
 </style>
