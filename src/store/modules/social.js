e34951fd5033817182b21fefcd5143e6f5c0c70a
Ben Garcia <ben@seebenprogram.com>
2018-04-02 13:06:55 -0400

    Add social icon data

--- src/store/modules/social.js
+++ src/store/modules/social.js
@@ -1,7 +1,15 @@
 const state = {
   _social: {
-    github: {},
-    linkedin: {}
+    github: {
+      href: 'https://github.com/BenRGarcia',
+      imgSrc: require('../../assets/social/GitHub.png'),
+      imgAlt: 'GitHub logo'
+    },
+    linkedin: {
+      href: 'https://www.linkedin.com/in/seebenprogram',
+      imgSrc: require('../../assets/social/LinkedIn.png'),
+      imgAlt: 'LinkedIn logo'
+    }
   }
 }
 
