c57a170394a1d4941326ed21796986b735c56b43
Ben Garcia <ben@seebenprogram.com>
2018-04-08 23:32:21 -0400

    Update portfolio links

--- src/store/modules/portfolio.js
+++ src/store/modules/portfolio.js
@@ -1,55 +1,25 @@
 const state = {
   _projects: [
     {
+      name: 'Multiplayer Trivia',
+      description: 'A realtime group trivia game',
+      src: require('../../assets/images/portfolio/vue.png'),
+      href: 'https://github.com/BenRGarcia/Multiplayer-Trivia-v2'
+    },
+    {
+      name: 'Tune Up',
+      description: 'A car maintenance utility app',
+      src: require('../../assets/images/portfolio/vue.png'),
+      href: 'https://github.com/BenRGarcia/Tune-Up'
+    },
+    {
+      name: 'Bamazon',
+      description: 'A Node.js command line app made for Customers, Managers, and Supervisors',
+      src: require('../../assets/images/portfolio/bamazon.png'),
+      href: 'https://github.com/BenRGarcia/Bamazon'
+    },
+    {
       name: 'Liri',
-      description: 'A Node.js command line app that mimics Siri.',
-      src: require('../../assets/images/portfolio/vue.png'),
-      href: 'https://github.com/BenRGarcia/liri-node-app'
-    },
-    {
-      name: 'Miri',
-      description: 'A Node.js command line app that mimics Siri.',
-      src: require('../../assets/images/portfolio/vue.png'),
-      href: 'https://github.com/BenRGarcia/liri-node-app'
-    },
-    {
-      name: 'Niri',
-      description: 'A Node.js command line app that mimics Siri.',
-      src: require('../../assets/images/portfolio/vue.png'),
-      href: 'https://github.com/BenRGarcia/liri-node-app'
-    },
-    {
-      name: 'Miri',
-      description: 'A Node.js command line app that mimics Siri.',
-      src: require('../../assets/images/portfolio/vue.png'),
-      href: 'https://github.com/BenRGarcia/liri-node-app'
-    },
-    {
-      name: 'Miri',
-      description: 'A Node.js command line app that mimics Siri.',
-      src: require('../../assets/images/portfolio/vue.png'),
-      href: 'https://github.com/BenRGarcia/liri-node-app'
-    },
-    {
-      name: 'Miri',
-      description: 'A Node.js command line app that mimics Siri.',
-      src: require('../../assets/images/portfolio/vue.png'),
-      href: 'https://github.com/BenRGarcia/liri-node-app'
-    },
-    {
-      name: 'Miri',
-      description: 'A Node.js command line app that mimics Siri.',
-      src: require('../../assets/images/portfolio/vue.png'),
-      href: 'https://github.com/BenRGarcia/liri-node-app'
-    },
-    {
-      name: 'Miri',
-      description: 'A Node.js command line app that mimics Siri.',
-      src: require('../../assets/images/portfolio/vue.png'),
-      href: 'https://github.com/BenRGarcia/liri-node-app'
-    },
-    {
-      name: 'Miri',
       description: 'A Node.js command line app that mimics Siri.',
       src: require('../../assets/images/portfolio/vue.png'),
       href: 'https://github.com/BenRGarcia/liri-node-app'
