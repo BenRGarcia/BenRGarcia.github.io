e08f3066a17fe9ced1383a6aa2a3645fefec4560
Ben Garcia <ben@seebenprogram.com>
2018-04-10 13:31:34 -0400

    Modify jumbotron, aesthetics

--- src/components/jumbotron/Jumbotron.vue
+++ src/components/jumbotron/Jumbotron.vue
@@ -1,9 +1,14 @@
 <template>
-  <div class="jumbotron jumbotron-fluid">
-    <div class="container">
-      <h1 class="display-3" v-html="displayTitle"></h1>
-      <hr class="my-4">
-      <p class="lead">{{ displayText }}</p>
+  <div class="row">
+    <div id="jumbotron-wrapper" class="col-12 px-0">
+        <div class="jumbotron jumbotron-fluid">
+          <div class="container">
+            <h1 class="display-2" v-html="displayTitle"></h1>
+            <h3 class="pt-1" v-html="displayText"></h3>
+            <hr>
+            <h3>Let's build something <em>incredible</em> together</h3>
+          </div>
+        </div>
     </div>
   </div>
 </template>
@@ -13,12 +18,23 @@ export default {
   data () {
     return {
       displayTitle: 'Hi,<br>I\'m Ben',
-      displayText: 'I\'m a Full-Stack JavaScript Engineer'
+      displayText: 'Full-Stack JavaScript Engineer',
+      imgSrc: require('../../assets/images/biking.jpg'),
+      imgAlt: 'Biking on the Virginia Creeper Trail'
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-
+#jumbotron-wrapper {
+  background: url('../../assets/images/background/dark_fish_skin.png');
+}
+.jumbotron {
+  background: none;
+  color: #eee;
+}
+hr {
+  border-top: 3px solid #eee;
+}
 </style>
