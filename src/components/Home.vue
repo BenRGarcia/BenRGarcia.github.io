5f638246527ef2bf7a07aff67f6391a9e917a2e5
Ben Garcia <ben@seebenprogram.com>
2018-04-10 17:27:24 -0400

    Add banners for each section

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -8,6 +8,19 @@
     >
       <Jumbotron/>
     </div>
+    <!-- About section -->
+    <h1 :style="cssBanner">About</h1>
+    <div class="col-12 mt-5">
+      <div class="container">
+        <h2 class="quote">
+          {{ aboutText }}
+        </h2>
+        <p class="h3 my-5" v-html="aboutParagraph">
+        </p>
+      </div>
+    </div>
+    <!-- Skills section -->
+    <h1 :style="cssBanner">Skills</h1>
     <!-- Container for 'Skills' -->
     <div
       class="container"
@@ -22,9 +35,18 @@ import Jumbotron from './jumbotron/Jumbotron'
 import SkillsCard from './skills/SkillsCard'
 
 export default {
+  props: [
+    'cssBanner'
+  ],
   components: {
     Jumbotron,
     SkillsCard
+  },
+  data () {
+    return {
+      aboutText: 'Full-stack JavaScript engineer ready to grow, adapt, and collaborate to build unsurpassed client solutions.',
+      aboutParagraph: '<strong class="display-4">Testing...</strong>'
+    }
   },
   mounted: function () {
     return this.$store.dispatch('collapseNavbar')
@@ -33,6 +55,11 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.quote {
+  color: #383838;
+  border-left: 6px solid #383838;
+  padding: 12px 0 12px 15px;
+}
 .card {
   margin-bottom: 15px;
   border: 2px solid #ff0000;
