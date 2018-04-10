05695944f6e36f1edc046fce2b446605bc7ea989
Ben Garcia <ben@seebenprogram.com>
2018-04-10 17:27:44 -0400

    add text to v-html

--- src/components/jumbotron/Jumbotron.vue
+++ src/components/jumbotron/Jumbotron.vue
@@ -3,10 +3,10 @@
     <div id="jumbotron-wrapper" class="col-12 px-0">
       <div class="jumbotron jumbotron-fluid">
         <div class="container">
-          <h1 class="display-2" v-html="displayTitle"></h1>
-          <h3 class="pt-1" v-html="displayText"></h3>
+          <h1 class="display-1" v-html="displayTitle"></h1>
+          <h2 class="pt-1" v-html="displayText"></h2>
           <hr>
-          <h3>Let's build something <em>incredible</em> together</h3>
+          <h2 v-html="quote"></h2>
         </div>
       </div>
     </div>
@@ -19,6 +19,7 @@ export default {
     return {
       displayTitle: 'Hi,<br>I\'m Ben',
       displayText: 'Full-Stack JavaScript&nbsp;Engineer',
+      quote: 'Let\'s&nbsp;build&nbsp;something <em>incredible</em>&nbsp;together',
       imgSrc: require('../../assets/images/biking.jpg'),
       imgAlt: 'Biking on the Virginia Creeper Trail',
       jumbotron: require('../../assets/images/background/dark_fish_skin.png')
